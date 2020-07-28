f3025c0431d15f3638b7962e3b2a00fc91d353b3
dwkeum <=>
2020-07-28 14:59:32 +0900

    날씨 api 연동

--- weather.js
+++ weather.js
@@ -1,4 +1,21 @@
+const weather = document.querySelector('.js-weather');
+
 const COORDS = 'coords';
+const api_key = '08f0d99a8e5b0451d8b2f89969173057';
+
+function getWeather(lat,lng){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${api_key}&units=metric`
+    )
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        const temperature = json.main.temp;
+        const placed = json.name;
+        weather.innerText = `${temperature} @ ${placed}`;
+    }); 
+}
 
 
 function saveCoords(coordsObj){
@@ -14,6 +31,7 @@ function handleGeoSuccess(position){
     }
 
     saveCoords(coordsObj);
+    getWeather(lat,long);
 }
 
 function handleGeoError(){
@@ -24,8 +42,18 @@ function askForCords(){
     navigator.geolocation.getCurrentPosition(handleGeoSuccess,handleGeoError);
 }
 
+function loadCoords(){
+    const loadedCords = localStorage.getItem(COORDS);
+    if(loadedCords === null){
+        askForCords();
+    }else{
+        const parserCoords = JSON.parse(loadedCords);
+        getWeather(parserCoords.lat,parserCoords.long);
+    }
+}
+
 function init(){
-    askForCords()
+    loadCoords()
 }
 
 init();
